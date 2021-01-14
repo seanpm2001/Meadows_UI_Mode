6000ae8527cb000ede4a48fc5746d1577e445f04
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-01-13 20:12:06 -0800

    SelectMode.py
    The script for selecting an interface mode for Meadows.

---
+++ GUI_Mode/SelectMode.py
@@ -0,0 +1,37 @@
+# Start of script
+'''
+Graphical mode selection for Meadows
+'''
+# Definition section
+guiModeCurrent = int(0)
+'''
+0 - CLI
+1 - GUI
+2 - ZUI
+3 - COMING SOON
+'''
+# Drivers
+""" Not yet available"""
+# Output
+print ("Select an interface mode for Meadows")
+print ("0 - Command Line Interface [CLI]")
+print ("1 - Graphical User Interface [GUI]")
+print ("2 - Zooming User Interface [ZUI]")
+print ("3 - MOD%%")
+print ("Rememeber that you can always change it back")
+modeSelect = int(input("M://UI_ID:"))
+try:
+   modeSelect = int(number)
+except ValueError:
+   print("Please enter a valid ID.")
+guiModeCurrent == modeSelect
+noMore = input("Press [ENTER] key to return to your Meadows session")
+print ("Exiting... if this window does not go away, kill the process with a process manager")
+break
+'''
+File info
+File type: Python script file (*.py)
+Line count including blank lines and compiler line): 38
+File version: 1 (Wednesday, January 13th 2021 at 8:12 pm)
+'''
+# End of script
